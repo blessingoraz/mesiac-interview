c94c96b083143de0499ba495c7a611a742fa25e9
Blessing Orazulume <orazulumeblessing@gmail.com>
2019-03-04 00:12:21 +0100

    Add redux store

--- client/src/index.js
+++ client/src/index.js
@@ -2,9 +2,14 @@ import './css/main.css';
 
 import React from 'react';
 import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import store from './js/config/store';
 
 import App from './js/components/App';
 
-render( <App/>,
-    document.getElementById('root')        
-  );
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
