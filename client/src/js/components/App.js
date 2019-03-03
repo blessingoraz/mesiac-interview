020b53df07b42bd84eaf166bea784665d27a9396
Blessing Orazulume <orazulumeblessing@gmail.com>
2019-03-03 21:53:47 +0100

    Add styling for app

--- client/src/js/components/App.js
+++ client/src/js/components/App.js
@@ -1,13 +1,26 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { Jumbotron, Button } from 'react-bootstrap';
+
+// Import bootstrap css
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 class App extends Component {
   render() {
     return (
       <div>
-        Hello
+        <Jumbotron>
+          <h1>Hello, world!</h1>
+          <p>
+            This is a simple hero unit, a simple jumbotron-style component for calling
+            extra attention to featured content or information.
+          </p>
+          <p>
+            <Button variant="primary">Learn more</Button>
+          </p>
+        </Jumbotron>;
       </div>
-    )
+    );
   }
 }
 
-export default  App;
+export default App;
